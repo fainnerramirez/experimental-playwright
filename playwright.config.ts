236f181a9737d759d5b0aa9751719d1ccfa0dfb8
fainnerramirez <fainnerramirez@gmail.com>
2025-05-05 13:18:53 -0500

    feat: add firefox

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,9 +18,9 @@ export default defineConfig({
   },
   projects: [
     {
-      name: 'chromium',
+      name: 'firefox',
       use: {
-        browserName: 'chromium',
+        browserName: 'firefox',
       },
     },
   ],
